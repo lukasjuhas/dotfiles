c6c00e2e084243f03d6a36c47f5e659308d689e5
Lukas Juhas <hello@lukasjuhas.com>
2016-12-16 11:55:02 +0000

    added ¯\_(ツ)_/¯

--- .aliases.sh
+++ .aliases.sh
@@ -69,3 +69,6 @@ alias l="ls -lF ${colorflag}"
 
 # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update'
+
+# ¯\_(ツ)_/¯
+alias shrug='echo "¯\_(ツ)_/¯" | pbcopy && echo "¯\_(ツ)_/¯ copied to clipboard."'
